bb581851a0d914cefc9e5ddf1ad5b0fed12a23c0
FrogCpp <161460644+FrogCpp@users.noreply.github.com>
2025-02-05 21:50:31 +0300

    init

---
+++ CodeFile1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static System.Console;
+
+namespace CharTest_csharp
+{
+    internal class CodeFile1
+    {
+        public void mainTwo()
+        {
+            Socket senderSocket = new Socket(AddressFamily.InterNetwork,
+                                             SocketType.Stream,
+                                             ProtocolType.Tcp);
+
+
+            senderSocket.Connect(IPAddress.Any, 4001);
+
+            Write("Client");
+
+            byte[] requestBytes = Encoding.ASCII.GetBytes(@$"Hello World!");
+
+            Write(requestBytes);
+
+            int bytesSent = 0;
+            while (bytesSent < requestBytes.Length)
+            {
+                bytesSent += senderSocket.Send(requestBytes, bytesSent, requestBytes.Length - bytesSent, SocketFlags.None);
+            }
+        }
+    }
+}
